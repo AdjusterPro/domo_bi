28ab23e25c8b16778ed0a72c18e5b8216beeebac
Ben Dunlap <ben@adjusterpro.com>
2019-06-12 10:39:48 -0500

    add DomoBI#list_datasets and DomoDataSet#query. While at it, add some supporting methods and refactor a bit. query test not yet passing but might be a Domo config problem //bdunlap

--- lib/domo_bi.rb
+++ lib/domo_bi.rb
@@ -5,28 +5,27 @@ class DomoBI
     def initialize(client_id, secret, scope, logger, debug=false)
         raise(DomoBIException, 'please provide a Client ID and Secret for Domo') if client_id.nil? or secret.nil?
         @logger = logger
+        @debug = debug
         @access_token = JSON.parse(
-            self.api_get(
+            self.get(
                 "/oauth/token?grant_type=client_credentials&scope=#{scope}",
                 { 'Authorization' => 'Basic ' + Base64.urlsafe_encode64( "#{client_id}:#{secret}" ).chomp }
             )
         )['access_token']
-        @debug = debug
     end
 
-    def api_get(path, headers = nil)
+    def request(path, headers = {})
+        headers['Authorization'] = "Bearer #{@access_token}" unless headers.has_key?('Authorization')
         path = "/v1#{path}" unless /^\/oauth/.match(path)
-        headers = { 'Authorization' => "Bearer #{@access_token}" } if headers.nil?
 
         url = "https://api.domo.com#{path}"
-        @logger.debug("fetching #{url}" + (@debug ? " with headers [#{headers.inspect}]" : ""))
 
-        output = ''
+        @logger.debug("requesting #{url}" + (@debug ? " with headers [#{headers.inspect}]" : ""))
+        
+        response = nil
         loop do
             begin
-                open(url, headers) do |f|
-                    f.each_line { |line| output += line }
-                end
+                response = yield(url, headers)
                 break
             rescue Exception => e
                 raise e unless /429/.match(e.message)
@@ -35,20 +34,54 @@ class DomoBI
                 next
             end
         end
-        output.tap { |o| @logger.debug("response: #{o.inspect}") if @debug }
+        response.tap { |r| @logger.debug("response: #{r.inspect}") if @debug }
+    end
+
+    def get(path, headers = {})
+        self.request(path, headers) do |url, final_headers|
+            output = ''
+            open(url, final_headers) do |f|
+                f.each_line { |line| output += line }
+            end
+            output
+        end
+    end
+
+    def post(path, payload, headers = {})
+        self.request(path, headers) do |url, final_headers|
+            uri = URI(url)
+            http = Net::HTTP.new(uri.host, uri.port)
+            http.use_ssl = true
+
+            req = Net::HTTP::Post.new(uri)
+            final_headers.each { |name, value| req['name'] = 'value' }
+            req.body = payload.to_json
+
+            http.request(req)
+        end
+    end
+    
+    def list_datasets
+        JSON.parse(self.get("/datasets"))
     end
 end
 
 class DomoDataSet < DomoBI
-    def initialize(client_id, secret, logger, set_id)
-        super(client_id, secret, 'data', logger)
+    def initialize(client_id, secret, logger, set_id, debug=false)
+        super(client_id, secret, 'data', logger, debug)
         @set_id = set_id
     end
 
-    def get_all(options = {})
+    def export(options = {})
         CSV.parse(
-            self.api_get("/datasets/#{@set_id}/data?includeHeader=true"),
+            self.get("/datasets/#{@set_id}/data?includeHeader=true"),
             { :headers => true }
+        )
+    end
+
+    def query(sql)
+        JSON.parse(
+            self.post("/datasets/query/execute/#{@set_id}", { 'sql': sql })
         )
     end
 end
--- test/domo-test.rb
+++ test/domo-test.rb
@@ -1,6 +1,14 @@
 require 'test-helper'
 
 class DomoTest < Test::Unit::TestCase
+    def dataset
+        @dataset ||= DomoDataSet.new(ENV['DOMO_CLIENT_ID'], ENV['DOMO_SECRET'], Logger.new(STDOUT), ENV['DOMO_TEST_DATA'], true)
+    end
+
+    def domo
+        @domo ||= DomoBI.new(ENV['DOMO_CLIENT_ID'], ENV['DOMO_SECRET'], 'data', Logger.new(STDOUT), true)
+    end
+
     def test_check_for_credentials
         assert_raise_message('please provide a Client ID and Secret for Domo') do
             DomoBI.new(nil,'fake-secret', 'data', Logger.new(STDOUT))
@@ -12,15 +20,23 @@ class DomoTest < Test::Unit::TestCase
     end
 
     def test_domo_connection
-        omit
-        assert_not_nil(DomoBI.new(ENV['DOMO_CLIENT_ID'], ENV['DOMO_SECRET'], 'data', Logger.new(STDOUT)))
+        omit # this is implied in the tests below
+        assert_not_nil(self.domo)
     end
 
-    def test_pull_data
-        dataset = DomoDataSet.new(ENV['DOMO_CLIENT_ID'], ENV['DOMO_SECRET'], Logger.new(STDOUT), ENV['DOMO_TEST_DATA'])
+    def test_list_datasets
+        assert_compare(1, '<=', self.domo.list_datasets.size)
+    end
+
+    def test_export_data
         assert_equal(
             ['col1', 'col2', 'col3'],
-            dataset.get_all[0].headers.first(3)
+            self.dataset.export[0].headers.first(3)
         )
+    end
+
+
+    def test_query_data
+        assert_compare(1, '<=', self.dataset.query('select * from `API Test Data`'))
     end
 end
